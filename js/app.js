c3f5ed12459d33e212fc7fbffdc3f71e6f17c806
Muhammad <muhammad.ali.72.27@gmail.com>
2020-03-14 23:15:22 -0700

    added UI class, canvas element context, and set canvas element to respond to view

--- js/app.js
+++ js/app.js
@@ -1,8 +1,30 @@
 DOMItems = {
-    toggleBanner: 'banner-up'
+    toggleBanner: '.banner-up',
+    game: '.gameScreen'
 }
 
+class UI{
+    constructor(){
+        let DOMItems = {
+            toggleBanner: '.banner-up',
+            game: '.gameScreen'
+        }
+        this.DOMItems = DOMItems;
+    }
 
-function init (){
-    setupListeners();
+    getCanvas(){
+        return document.querySelector(this.DOMItems.game);
+    }
 }
+
+uiController = new UI();
+const canvas = uiController.getCanvas();
+//Give a little bit of space for the canvas
+canvas.width = window.innerWidth -10;
+canvas.height = window.innerHeight -20;
+const ctx = canvas.getContext('2d');
+
+//The Ludo game is a 15x15 matrix
+const xUnit = canvas.width/15;
+const yUnit = canvas.width/15;
+console.log(ctx);
