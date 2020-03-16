83c04547e8fc0310938926fc9a6057cf148da8bb
Muhammad <muhammad.ali.72.27@gmail.com>
2020-03-15 22:13:57 -0700

    added basic outline of the board

--- js/app.js
+++ js/app.js
@@ -13,10 +13,36 @@ class UI{
 }
 
 class Board{
-    constructor(xunit, yunit){
+    constructor(xunit, yunit, ctx){
         this.x = xunit;
         this.y = yunit;
+        this.ctx = ctx;
     }
+
+    makeRectangle(color, x, y, width, height){
+        this.ctx.strokeStyle = 'black';
+        this.ctx.lineWidth = 2;
+        this.ctx.fillStyle = color;
+        this.ctx.fillRect(x,y, width, height);
+        this.ctx.strokeRect(x, y, width, height);
+    }
+
+    setupBoard(){
+        const bigX = this.x*6;
+        const bigY = this.y*6;
+        const [homeX, homeY] = [this.x*3, this.y*3];
+
+        let homeImg = new Image();
+        homeImg.src = './resources/piece.png';
+        console.log(homeImg);
+        console.log(this.ctx.drawImage(homeImg, 0, 0));
+        this.makeRectangle('#004d1a', 0, 0, bigX, bigY);
+        this.makeRectangle('#4d0f00', 9*this.x, 0, bigX, bigY);
+        this.makeRectangle('#00004d', 0, 9*this.y, bigX, bigY);
+        this.makeRectangle('#4d4d00', 9*this.x, 9*this.y, bigX, bigY);
+        this.makeRectangle('white', 2*homeX, 2*homeY, homeX, homeY);
+    }
+
 }
 
 class MainController{
@@ -30,18 +56,20 @@ class MainController{
         this.ctx = ctx;
 
         //The Ludo game is a 15x15 matrix
-        const xUnit = canvas.width/15;
-        const yUnit = canvas.height/15;
+        const xUnit = Math.round(canvas.width/15);
+        const yUnit = Math.round(canvas.height/15);
         return [xUnit, yUnit];
     }
 
     init(){
         this.uiCtl = new UI();
-        this.setupCanvas();
-        
-
+        const [dx, dy] = this.setupCanvas();
+        this.boardCtl = new Board(dx, dy, this.ctx);
+        this.boardCtl.setupBoard();
+        // console.log(`${dx} + ${dy}`);
     }
 }
 
 mainCtl = new MainController();
+ctx = mainCtl.ctx;
 mainCtl.init();
