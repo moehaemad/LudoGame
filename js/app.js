96f1ef1ffc65d22111f5ac50118df98bfcb75c8b
Muhammad <muhammad.ali.72.27@gmail.com>
2020-03-15 23:26:10 -0700

    added the paths on the board

--- js/app.js
+++ js/app.js
@@ -27,11 +27,25 @@ class Board{
         this.ctx.strokeRect(x, y, width, height);
     }
 
+    makePath(x, y, iterations, incDirec, color = 'white'){
+        for (let i = 0; i <= iterations; i++){
+            if (incDirec == 'vertical'){
+                this.makeRectangle(color, x, y + (this.y*i), this.x, this.y)
+            }
+            if (incDirec === 'horizontal'){
+                this.makeRectangle(color, x + (this.x * i), y, this.x, this.y);
+            }
+        }
+
+    }
+
     setupBoard(){
+        //All main boxes for not playing players is a 6 unti square.
         const bigX = this.x*6;
         const bigY = this.y*6;
+        //Make unit calc easier for home square.
         const [homeX, homeY] = [this.x*3, this.y*3];
-
+        //TODO: eventually add in image for home
         let homeImg = new Image();
         homeImg.src = './resources/piece.png';
         console.log(homeImg);
@@ -40,7 +54,25 @@ class Board{
         this.makeRectangle('#4d0f00', 9*this.x, 0, bigX, bigY);
         this.makeRectangle('#00004d', 0, 9*this.y, bigX, bigY);
         this.makeRectangle('#4d4d00', 9*this.x, 9*this.y, bigX, bigY);
-        this.makeRectangle('white', 2*homeX, 2*homeY, homeX, homeY);
+        this.makeRectangle('pink', 2*homeX, 2*homeY, homeX, homeY);
+        // Draw all vertical paths
+        this.makePath(this.x*6, 0, 5, 'vertical');
+        this.makePath(this.x*7, 0, 5, 'vertical');
+        this.makePath(this.x*8, 0, 5, 'vertical');
+        this.makePath(this.x*6, this.y*9, 5, 'vertical');
+        this.makePath(this.x*7, this.y*9, 5, 'vertical');
+        this.makePath(this.x*8, this.y*9, 5, 'vertical');
+        this.makePath(0, this.y*6, 5, 'horizontal');
+        this.makePath(0, this.y*7, 5, 'horizontal');
+        this.makePath(0, this.y*8, 5, 'horizontal');
+        this.makePath(this.x*9, this.y*6, 5, 'horizontal');
+        this.makePath(this.x*9, this.y*7, 5, 'horizontal');
+        this.makePath(this.x*9, this.y*8, 5, 'horizontal');
+    }
+
+    clearScreen(){
+        this.ctx.clearRect(0, 0, window.innerWidth, window.innerHeight);
+        this.setupBoard();
     }
 
 }
