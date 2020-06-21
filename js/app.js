10e865d12b251939dd9f3e98e13ca4da3b7b8720
Muhammad <muhammad.ali.72.27@gmail.com>
2020-06-20 21:22:33 -0700

    added webpack to make js smaller to host on website

--- js/app.js
+++ js/app.js
@@ -777,7 +777,7 @@ mainCtl.init();
 
 mainCtl.addPlayer();
 mainCtl.boardCtl.coord.green[3] = [0,7];
-// mainCtl.quad.illegalValues.yellow = true;
+mainCtl.quad.illegalValues.yellow = true;
 mainCtl.quad.illegalValues.green = true;
 mainCtl.diceRoll = 5;
 // mainCtl.quad.illegalValues['green'] = true;
